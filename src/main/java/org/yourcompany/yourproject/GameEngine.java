9a0a6d7d70ccabead00c531ebbbb05dadd4b6642
Ben <benjamin.e.richards@gmail.com>
2024-11-10 18:07:28 -0500

    Initial commit, basic ideas

---
+++ src/main/java/org/yourcompany/yourproject/GameEngine.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author benazoid
+ */
+public class GameEngine {
+
+     
+    /*
+    
+    Structure
+    - Scene
+    - Camera (GameObject, class of own, or just vec2 in the scene?)
+    - GameObject
+    - Physics (collider)
+    - Dialogue/hud
+
+    Rendering
+    Overarching 2D sprite drawer class (name not chosen)
+    - Drawing polygons (from mesh (basically))
+        Shaders?
+    - Sprites/spritesheets (from texture)
+        I want this to be an option but not the main one
+
+
+    Who has access to the graphics object?
+    - Make it very accessable, but easier to fuck up
+    - Or make it very private and secure
+
+    Event based
+
+    How does dialouge work?
+    - its own class? probably
+    - id like it to be very flexible
+
+    How to give an object code of its own?
+    - Lambda stuff
+
+    How do i want general code while in developement to feel?
+
+     */
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/main/java/org/yourcompany/yourproject/GameObject.java
@@ -0,0 +1,21 @@
+package org.yourcompany.yourproject;
+
+/*
+What are the essentials of a GameObject?
+- Position
+- Rotation
+- 
+
+Things that would be game objects
+- Player
+- Movables
+
+Things that might be game objects
+- Things that can't be interected with
+- Things that never change (walls, etc)
+
+ */
+
+public class GameObject {
+    
+}
